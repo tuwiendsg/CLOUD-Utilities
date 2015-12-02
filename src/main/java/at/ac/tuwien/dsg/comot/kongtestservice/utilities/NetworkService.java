a0eae8dae2d40d222725f96f2c20ea4be6658c34
Svetoslav Videnov <svetoslav.videnov@dsg.tuwien.ac.at>
2015-12-02 11:20:02 +0100

    improved network service

--- src/main/java/at/ac/tuwien/dsg/comot/kongtestservice/utilities/NetworkService.java
+++ src/main/java/at/ac/tuwien/dsg/comot/kongtestservice/utilities/NetworkService.java
@@ -20,28 +20,30 @@ import org.slf4j.LoggerFactory;
  * @author Svetoslav Videnov <s.videnov@dsg.tuwien.ac.at>
  */
 public class NetworkService {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(NetworkService.class);
 
 	public static String getIp() throws SocketException {
 		for (NetworkInterface nic : Collections.list(NetworkInterface.getNetworkInterfaces())) {
-			for (InetAddress ad : Collections.list(nic.getInetAddresses())) {
-				//test internet connection
-				try (SocketChannel socket = SocketChannel.open()) {
-					socket.socket().setSoTimeout(5000);
+			if (!nic.isLoopback() && !nic.isVirtual() && nic.isUp()) {
+				for (InetAddress ad : Collections.list(nic.getInetAddresses())) {
+					//test internet connection
+					try (SocketChannel socket = SocketChannel.open()) {
+						socket.socket().setSoTimeout(5000);
 
-					socket.bind(new InetSocketAddress(ad, 0));
-					socket.connect(new InetSocketAddress("google.com", 80));
-					//if everything passes the InetAddress should be okay.
-					socket.close();
-					String ip = ad.getHostAddress();
-					logger.trace("Found interface: {}", ip);
-					return ip;
-				} catch (IOException ex) {
+						socket.bind(new InetSocketAddress(ad, 0));
+						socket.connect(new InetSocketAddress("google.com", 80));
+						//if everything passes the InetAddress should be okay.
+						socket.close();
+						String ip = ad.getHostAddress();
+						logger.trace("Found interface: {}", ip);
+						return ip;
+					} catch (IOException ex) {
+					}
 				}
 			}
 		}
-		
+
 		return null;
 	}
 }
